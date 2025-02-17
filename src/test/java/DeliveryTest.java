26fd840c117d481c028701a4ee35af924e48ca25
osukhorukova <o.skhrkv@gmail.com>
2025-02-17 15:06:35 +0100

    New unit tests

--- src/test/java/DeliveryTest.java
+++ src/test/java/DeliveryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Validate;
@@ -30,31 +31,46 @@ public class DeliveryTest {
     @DisplayName("Maximum delivery price")
     void maximumDeliveryPriceTest() {
         Delivery delivery = new Delivery(30, CargoDimension.LARGE, true, ServiceWorkload.VERY_HIGH);
-        assertEquals(1120, delivery.calculateDeliveryCost());
-    }
-
-    /*
-    static Stream<Integer> provideDistance(){
-        return Stream.of(2,3,9,10,11,29);
+        double actualPrice = delivery.calculateDeliveryCost();
+        assertEquals(1120, actualPrice);
     }
 
     @ParameterizedTest
-    @MethodSource("provideDistance")
     @Tag("Positive")
-    @DisplayName("Distance boundary Values")
-    void distanceBoundaryValuesTest(int distance) {
-        List<Integer> expectedList = Arrays.asList()
-        Delivery delivery = new Delivery(distance, CargoDimension.LARGE, true, ServiceWorkload.VERY_HIGH);
-        assertEquals(1120, delivery.calculateDeliveryCost());
+    @DisplayName("Distance boundary values")
+    @CsvSource({
+            "2,SMALL,false,NORMAL,400.0",
+            "9,LARGE,true,HIGH,840.0",
+            "10,SMALL,false,NORMAL,400.0",
+            "11,LARGE,true,HIGH,980.0",
+            "29,SMALL,true,VERY_HIGH,960.0",
+            "30,LARGE,false,INCREASED,480.0",
+    })
+    void distanceBoundaryValuesTest(int distance, CargoDimension cargoDimension, boolean isFragile, ServiceWorkload serviceWorkload, double expectedPrice) {
+        Delivery delivery = new Delivery(distance, cargoDimension, isFragile, serviceWorkload);
+        double actualPrice = delivery.calculateDeliveryCost();
+        assertEquals(expectedPrice, actualPrice);
     }
-     */
 
     @Test
     @Tag("Positive")
-    @DisplayName("The cargo is fragile and the distance is less than 30 km")
+    @DisplayName("The cargo is fragile and the distance is 30 km")
     void fragileAndShortDistancePriceTest() {
         Delivery delivery = new Delivery(30, CargoDimension.SMALL, true, ServiceWorkload.INCREASED);
-        assertEquals(720, delivery.calculateDeliveryCost());
+        double actualPrice = delivery.calculateDeliveryCost();
+        assertEquals(720, actualPrice);
+    }
+
+    @Test
+    @Tag("Negative")
+    @DisplayName("Workload cannot be null")
+    void nullPointerExceptionTest() {
+        Delivery delivery = new Delivery(1, CargoDimension.SMALL, false, null);
+        Throwable exception = assertThrows(
+                NullPointerException.class,
+                delivery::calculateDeliveryCost
+        );
+        assertEquals("Cannot invoke \"olenzing.ServiceWorkload.getDeliveryRate()\" because \"this.deliveryServiceWorkload\" is null", exception.getMessage());
     }
 
     @Test
